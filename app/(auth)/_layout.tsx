cde8d51c4395843e73267e35cbd6a2b9d268f045
Vanh Tran <vanhtran18.soict@gmail.com>
2025-03-04 14:56:22 +0700

    Designing auth page

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,13 @@
+import "../../global.css"
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const AuthLayout = () => {
+  return (
+    <View>
+      <Text>AuthLayout</Text>
+    </View>
+  )
+}
+
+export default AuthLayout
