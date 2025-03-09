ffdd6adf1f0355135aea7a435e991f47ae8044c0
Vanh Tran <vanhtran18.soict@gmail.com>
2025-03-10 00:05:36 +0700

    Save login/logout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,9 +3,11 @@ import { Text, View } from 'react-native'
 import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 
+import GlobalProvider from '../../context/GlobalProvider'
+
 const AuthLayout = () => {
   return (
-    <>
+    <GlobalProvider>
       <Stack>
         <Stack.Screen 
           name='sign-in'
@@ -24,7 +26,7 @@ const AuthLayout = () => {
 
       <StatusBar backgroundColor="#161662"
       style='light' />
-    </>
+    </GlobalProvider>
   )
 }
 
